075e62241486afff22b83c2dbf389998776e66e4
Snehan Peshin <snehanpeshin@Snehans-MacBook-Air.local>
2026-07-13 18:53:02 -0400

    Add AdSense verification and ads.txt

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -54,6 +54,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-6605747981994820" />
+        <script
+          async
+          crossOrigin="anonymous"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6605747981994820"
+        />
+      </head>
       <body>
         <Script
           async
